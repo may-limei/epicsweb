7d0cad5352b7312b1149d1f3f84946b556b9dc75
may-limei <limei_wu@yeah.net>
2018-09-19 15:38:35 +0800

    综合实验平台侧边栏收起

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get('/widget_led', function(req, res, next) {
   res.render('widget_led', { title: 'Widget led' });
 });
 
-router.get('/system0.html', function(req, res, next) {
+router.get('/system0', function(req, res, next) {
   res.render('system0', { title: 'system-0' });
 });
 
